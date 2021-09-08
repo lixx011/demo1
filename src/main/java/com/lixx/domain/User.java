00989088abd75f8067604b649d77722e05cf0832
lixx <lixx@bupt.edu.cn>
2021-09-08 22:20:08 +0800

    first commit by idea

---
+++ src/main/java/com/lixx/domain/User.java
@@ -0,0 +1,19 @@
+package com.lixx.domain;
+
+/**
+ * @description;
+ * @author: lixx;
+ * @date: 2021/9/8 下午10:10;
+ */
+public class User {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/lixx/service/UserService.java
@@ -0,0 +1,13 @@
+package com.lixx.service;
+
+/**
+ * @description;
+ * @author: lixx;
+ * @date: 2021/9/8 下午10:11;
+ */
+public class UserService {
+
+    public void saveUser(){
+        System.out.println("saveUser");
+    }
+}
